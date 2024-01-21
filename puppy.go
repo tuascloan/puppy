1ae2c85316b07b9bfd4f4bdc4dd6de6ddccb39ad
Frode Eriksen <frode@eriksen.casa>
2024-01-21 17:55:13 +0100

    version 1.1.0

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/tuascloan/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func From11() {
+	fmt.Println("I´m from version 1.1.0")
 }
